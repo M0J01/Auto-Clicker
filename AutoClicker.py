aa500dd6af582d5f54c734bcc24d1ce7ea9795ba
M0J01 <moodymusicman@gmail.com>
2019-04-13 21:59:04 -0400

    Added a version for the Ri

---
+++ AutoClicker.py
@@ -0,0 +1,25 @@
+from pymouse import PyMouse
+import time
+import math
+
+m = PyMouse()
+
+def click(x,y):
+	m.click(x,y,1)
+
+
+
+click(1600,750)
+
+delay = 10
+seconds = 60
+minutes = 60
+
+numHours = .5
+
+totalTime = numHours*minutes*seconds
+numLikes = totalTime/delay
+
+for i in range(int(round(numLikes))):
+    time.sleep(delay)
+    click(1600,750)
